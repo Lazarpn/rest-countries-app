05f9092504868d9faa1f1e3ca35621d71b3e9671
Lazar <lazarst.pn@gmail.com>
2024-03-05 21:27:09 +0100

    Another changes

--- src/app/+countries/countries-list/countries-list.component.ts
+++ src/app/+countries/countries-list/countries-list.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class CountriesListComponent implements OnInit {
+  countries = [
+    { population: 80000, region: 'Balkan', capital: 'Belgrade' },
+    { population: 70000, region: 'Balkan', capital: 'Novi Sad' },
+    { population: 50000, region: 'Balkan', capital: 'Subotica' },
+    { population: 30000, region: 'Balkan', capital: 'Paracin' },
+  ];
+
   constructor() { }
 
   ngOnInit() { }
