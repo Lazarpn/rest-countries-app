754e3beccd134c0f7a9f738e3f64f92546e4920e
Lazar <lazarst.pn@gmail.com>
2024-03-05 20:12:28 +0100

    Another changes

--- src/app/rc.module.ts
+++ src/app/rc.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { RcComponent } from './rc.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    RcComponent
+    RcComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
